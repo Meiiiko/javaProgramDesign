ee520e6d1048067f736aa994bbb9100838e599aa
Meiiiko <2019214118@stu.cqupt.edu.cn>
2021-03-22 19:34:26 +0800

    第一次课自学

---
+++ src/Extend.java
@@ -0,0 +1,19 @@
+class MyThread extends Thread{
+    private String name;
+    public MyThread(String name){
+        this.name=name;
+    }
+    public void run(){
+        for(int i = 0;i<10000;i++){
+            System.out.println(name+"运行，i="+i);
+        }
+    }
+}
+public class Extend {
+    public static void main(String args[]){
+        MyThread threadOne = new MyThread("线程A");
+        MyThread threadTwo = new MyThread("线程B");
+        threadOne.start();
+        threadTwo.start();
+    }
+}
---
+++ src/Runable.java
@@ -0,0 +1,2 @@
+public class Runable {
+}
